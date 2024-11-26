651ae6c03ebd3e5b4cac7cd6124d719a466d4243
Paulo Bettencourt <pauloblemos@gmail.com>
2024-11-26 22:33:45 -0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SearchComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'countries-api';
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed, effect, resource } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface Country {
+  name: {
+    common: string;
+  };
+}
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  imports: [
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
+})
+export class SearchComponent {
+  countryNames = computed(() => this.countries.value());
+  PARAM_KEYWORD = 'name';
+  constructor() {
+    effect(() => {
+      console.log('countryNames:', this.countryNames());
+    });
+  }
+  async fetchCountries(key: string) {
+    const res = await fetch(`https://restcountries.com/v3.1/all?fields=${key}`);
+    return res.json() as Promise<Country[]>;
+  }
+  // IF PARAM KEYWORD CHANGES, IT WILL MAKE A NEW API REQUEST
+  countries = resource<Country[], string>({
+    // Data type is Country[] (loader) and string concerns the "request"
+    request: () => this.PARAM_KEYWORD,
+    loader: ({ request }) => this.fetchCountries(request),
+  });
+}
