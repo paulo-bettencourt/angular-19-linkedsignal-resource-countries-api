bf5fadbbd206737763cd22c443872aada93dc665
Paulo Bettencourt <pauloblemos@gmail.com>
2024-11-27 14:17:53 -0100

    signals refactor

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,17 +4,26 @@ import {
   effect,
   linkedSignal,
   resource,
+  signal,
 } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
 
 export interface Country {
   name: {
     common: string;
+    official: string;
   };
 }
+
+export type CountryName = keyof {
+  common: string;
+  official: string;
+};
 
 @Component({
   selector: 'app-search',
@@ -25,12 +34,20 @@ export interface Country {
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
+    MatRadioModule,
+    FormsModule,
   ],
 })
 export class SearchComponent {
   countryNames = computed(() => this.countries.value());
   selectedOption = linkedSignal(() => this.countries.value());
   PARAM_KEYWORD = 'name';
+  radioValue: CountryName = 'common';
+  selectedCountry = signal('');
+
+  constructor() {
+    effect(() => {});
+  }
 
   async fetchCountries(key: string) {
     const res = await fetch(`https://restcountries.com/v3.1/all?fields=${key}`);
@@ -45,12 +62,25 @@ export class SearchComponent {
 
   onCountrySearch(event: Event) {
     const inputElement = event.target as HTMLInputElement;
-    const countryFiltered = this.countryNames()?.filter((country) =>
-      country.name.common
-        .toLowerCase()
-        .includes(inputElement.value.toLowerCase())
-    );
-    this.selectedOption.set(countryFiltered);
-    console.log('country: ', countryFiltered);
+    if (inputElement.value) {
+      const countryFiltered = this.countryNames()?.filter((country) =>
+        country.name[this.radioValue]
+          .toLowerCase()
+          .includes(inputElement.value.toLowerCase())
+      );
+      console.log('countryFiltered: ', countryFiltered);
+      this.selectedOption.set(countryFiltered);
+    } else {
+      this.selectedCountry.set('');
+    }
+  }
+
+  onCountrySelect(country: string) {
+    this.selectedCountry.set(country);
+    this.selectedOption.set(this.countryNames());
+  }
+
+  onSelectionChange(value: CountryName): void {
+    this.radioValue = value;
   }
 }
